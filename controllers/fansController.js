781173444d68c4a525da702a874a16470d1619a8
temilolu97 <temiloluwaabiodunojo@gmail.com>
2024-11-21 14:08:20 +0100

    changes

--- controllers/fansController.js
+++ controllers/fansController.js
@@ -227,7 +227,7 @@ const generateRegistrationLink = async (req, res) => {
         const newLink = await prisma.fanRegistrationLinks.create({
             data: {
                 clubId: user.teamId,
-                registrationLink: `http://localhost:5173/fans/registration/${token}`,
+                registrationLink: `${frontendUrl}/fans/registration/${token}`,
                 token: token,
                 isActive: true,
                 dateCreated: new Date(),
